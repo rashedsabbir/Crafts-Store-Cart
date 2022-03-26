adc51ec3272802c785cd9697cc7aae00d2e0a0dd
rashedsabbir <rashedulhasan1996@gmail.com>
2022-03-26 20:02:29 +0600

    products added

---
+++ src/components/product/product.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import "./Expert.css";
+
+const Expert = (props) => {
+    // destructuring
+    const { name, image, price, catagory, size, delivery } = props.expert || {};
+
+    // adding jsx
+    return (
+
+        <div className="card">
+            <div className="card-img">
+                <img src={image} alt="" />
+            </div>
+            <div className="card-info">
+                <h3>{name}</h3>
+                <p>
+                    <small>Size: {size}</small>
+                </p>
+                <p>
+                    <small>(Delivery Time: {delivery})</small>
+                </p>
+                <h5>
+                    Catagory: <i>{catagory}</i>{" "}
+                </h5>
+                <h4>Prcie: ${price}</h4>
+            </div>
+            {/* button for add to cart */}
+            <div>
+                <button
+                    onClick={() => props.handleHire(props.expert)}
+                    className="card-btn"
+                >
+                    <FontAwesomeIcon className="cart_icon" icon={faShoppingCart} /> Add To Cart
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default Expert;
